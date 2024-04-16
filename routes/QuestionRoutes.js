098e58d8fbc613718d9bb55f0c9dcdf858153a38
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-04-16 11:17:03 -0300

    feat(express validator in update)

--- routes/QuestionRoutes.js
+++ routes/QuestionRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const { createQuestion, getQuestions, getQuestionsByPhase, updateQuestion, deleteQuestion, checkAnswer } = require("../controllers/QuestionController");
 const authGuard = require("../middlewares/authGuard");
-const { questionCreateValidation } = require("../middlewares/questionValidation");
+const { questionCreateValidation, updateQuestionValidation } = require("../middlewares/questionValidation");
 const validate = require("../middlewares/handleValidation");
 
 router.post("/create", authGuard, questionCreateValidation(), validate, createQuestion);
 router.post("/check/:id", authGuard, checkAnswer);
 router.get("/", authGuard, getQuestions)
 router.get("/byPhase", authGuard, getQuestionsByPhase);
-router.put("/update/:id", authGuard, updateQuestion);
+router.put("/update/:id", authGuard, updateQuestionValidation(), validate, updateQuestion);
 router.delete("/:id", authGuard, deleteQuestion);
 
 module.exports = router;
